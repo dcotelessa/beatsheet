fb8b056e9ca9198b654c4101e71fd68ffeb433be
dcotelessa <david@cotelessa.com>
2024-05-24 19:14:49 -0700

    update context to ts

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,4 +1,12 @@
-import { createContext } from "react";
+import { createContext, Dispatch } from "react";
+import { BeatSheetProps } from '@/components/BeatSheet';
 
-export const BeatSheetContext = createContext(null);
-export const BeatSheetDispatchContext = createContext(null);
+type BeatSheetAction = {
+  type: string;
+  payload: any; // Replace 'any' with the shape of your action payload
+};
+
+export const BeatSheetContext = createContext<BeatSheetProps | null>(null);
+export const BeatSheetDispatchContext =
+  createContext<Dispatch<BeatSheetAction> | null>(null);
+
